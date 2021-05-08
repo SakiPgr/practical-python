9372474ffceb5ceed9fbad8f00ed6b527f758e4d
SakiPgr <realsaki@hotmail.gr>
2021-05-08 10:10:26 +0300

    Finished exercise 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,13 @@
 # pcost.py
 #
 # Exercise 1.27
+with open('Data/portfolio.csv', 'rt') as file:
+    data = file.readlines()
+
+sum = 0
+
+for line in data[1:]:
+    line_data = line.strip().split(',')
+    sum += int(line_data[1]) * float(line_data[2])
+
+print(f"Total cost {sum}")
